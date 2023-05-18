a4ed43752e177dbcd3af3ed3d83e8a1d5bcaa683
OmarThinks <24939461+OmarThinks@users.noreply.github.com>
2023-05-18 04:00:37 +0300

    eslint: starndard is the best

--- 0.71.8/AwesomeProject/.eslintrc.js
+++ 0.71.8/AwesomeProject/.eslintrc.js
@@ -1,31 +1,24 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
   extends: [
-    // '@react-native-community',
     'plugin:react/recommended',
-    'airbnb',
+    'standard'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 'latest',
-    sourceType: 'module',
+    sourceType: 'module'
   },
-  plugins: ['react', '@typescript-eslint'],
+  plugins: [
+    'react',
+    '@typescript-eslint'
+  ],
   rules: {
-    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
-    'import/extensions': [
-      {
-        js: 'always',
-        jsx: 'always',
-        ts: 'always',
-        tsx: 'always',
-      },
-    ],
-  },
-};
+  }
+}
--- 0.71.8/AwesomeProject/App.tsx
+++ 0.71.8/AwesomeProject/App.tsx
@@ -1,29 +1,28 @@
-import React from 'react';
-import {
-  SafeAreaView, ScrollView, StatusBar, Text,
-} from 'react-native';
-import { PaperProvider } from 'react-native-paper';
-import { lightTheme } from './src/theme';
-import { darkTheme } from '@root/src/theme';
+import React from 'react'
+import { SafeAreaView, ScrollView, StatusBar, Text } from 'react-native'
+import { PaperProvider } from 'react-native-paper'
+import { lightTheme } from './src/theme'
 
-
-//console.log(darkTheme);
-
-function App(): React.JSX.Element {
+function App (): React.JSX.Element {
   const backgroundStyle = {
-    backgroundColor: 'white',
-  };
+    backgroundColor: 'white'
+  }
 
   return (
     <PaperProvider theme={lightTheme}>
       <SafeAreaView style={backgroundStyle}>
-        <StatusBar barStyle="dark-content" backgroundColor={backgroundStyle.backgroundColor} />
-        <ScrollView contentInsetAdjustmentBehavior="automatic" style={backgroundStyle}>
+        <StatusBar
+          barStyle="dark-content"
+          backgroundColor={backgroundStyle.backgroundColor}
+        />
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={backgroundStyle}>
           <Text>Hey</Text>
         </ScrollView>
       </SafeAreaView>
     </PaperProvider>
-  );
+  )
 }
 
-export default App;
+export default App
