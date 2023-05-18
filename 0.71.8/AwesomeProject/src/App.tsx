3e1a21751162d99f51c4e15d977e57db1c4203f3
OmarThinks <24939461+OmarThinks@users.noreply.github.com>
2023-05-19 00:45:47 +0300

    React Navigation: implemented

--- 0.71.8/AwesomeProject/src/App.tsx
+++ 0.71.8/AwesomeProject/src/App.tsx
@@ -4,13 +4,23 @@ import React from 'react';
 import { PaperProvider } from 'react-native-paper';
 import { Provider as ReduxProvider, useSelector } from 'react-redux';
 import { Home } from '@screens';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const AppInsideRNP = () => {
+const Stack = createNativeStackNavigator();
+
+const AppWithoutRedux = () => {
   const theme = useSelector(themeSelector);
 
   return (
     <PaperProvider theme={theme === 'light' ? lightTheme : darkTheme}>
-      <Home />
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </PaperProvider>
   );
 };
@@ -18,7 +28,7 @@ const AppInsideRNP = () => {
 const App = () => {
   return (
     <ReduxProvider store={store}>
-      <AppInsideRNP />
+      <AppWithoutRedux />
     </ReduxProvider>
   );
 };
