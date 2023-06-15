9117b2a8423a5a38c22a158899cc3c57f84354fe
OmarThinks <24939461+OmarThinks@users.noreply.github.com>
2023-06-15 07:27:59 +0300

    MainLayout: prepared, I guess

--- 0.71.8/AwesomeProject/src/hoc/MainLayout/MainLayout.tsx
+++ 0.71.8/AwesomeProject/src/hoc/MainLayout/MainLayout.tsx
@@ -1,12 +1,31 @@
 import { View, Text } from 'react-native';
 import React from 'react';
 
-const MainLayout = () => {
-  return (
-    <View>
-      <Text>MainLayout</Text>
-    </View>
-  );
+const MainLayout = (
+  ScreenComponent: React.FC,
+  {
+    scrollable = true
+  }: {
+    scrollable?: boolean;
+  }
+) => {
+  const InnerMainLayout = () => {
+    return (
+      <View>
+        <Text>MainLayout</Text>
+        <ScreenComponent />
+      </View>
+    );
+  };
+
+  return InnerMainLayout;
 };
 
 export default MainLayout;
+
+/*
+I want MainLayout to be used like this:
+
+return MainLayout(Home, {})
+
+*/
