b21ce79b02db7415b5616c4bf796974447c6e3bb
OmarThinks <24939461+OmarThinks@users.noreply.github.com>
2023-06-15 18:10:26 +0300

    AppBar: more styling

--- 0.71.8/AwesomeProject/src/components/AppBar/AppBar.tsx
+++ 0.71.8/AwesomeProject/src/components/AppBar/AppBar.tsx
@@ -6,13 +6,39 @@ const AppBar = () => {
   const colors = useAppTheme().colors;
 
   return (
-    <View style={{ height: 50, backgroundColor: colors.appBarBg }}>
-      <Text
-        style={{
-          color: colors.normalText
-        }}>
-        AppBar
-      </Text>
+    <View
+      style={{
+        height: 50,
+        backgroundColor: colors.appBarBg,
+        paddingHorizontal: 10,
+        alignSelf: 'stretch',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+      }}>
+      <View>
+        <Text
+          style={{
+            color: colors.normalText
+          }}>
+          AppBar
+        </Text>
+      </View>
+      <View style={{ flexDirection: 'row' }}>
+        <Text
+          style={{
+            color: colors.normalText,
+            marginRight: 10
+          }}>
+          Drawer
+        </Text>
+        <Text
+          style={{
+            color: colors.normalText
+          }}>
+          Theme
+        </Text>
+      </View>
     </View>
   );
 };
