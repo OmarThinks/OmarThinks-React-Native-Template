35664f6f5a49b4968b4f05ad8b86dc902a45ddbd
OmarThinks <24939461+OmarThinks@users.noreply.github.com>
2023-06-15 06:42:04 +0300

    Fonts: edited
    I know that the code is bigger now,
    but it is clear very clear

--- 0.71.8/AwesomeProject/src/theme/fonts.tsx
+++ 0.71.8/AwesomeProject/src/theme/fonts.tsx
@@ -1,42 +1,205 @@
 import { DefaultTheme } from 'react-native-paper';
 
-const fontNumbers = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
-  23, 24, 25
-] as const;
-
-type FontNumbers = (typeof fontNumbers)[number];
-
-type CustomFonts = {
-  [k in FontNumbers as `v${k}`]: {
-    fontSize: k;
-    fontWeight: 'bold';
-  };
-} & {
-  [k in FontNumbers as `v${k}n`]: {
-    fontSize: k;
-    fontWeight: 'normal';
-  };
-};
-
-const additionalFonts = fontNumbers.reduce((acc, value) => {
-  // console.log(value);
-  // console.log(typeof value);
-
-  acc[`v${value}` as `v${1}`] = {
-    fontSize: fontNumbers[value - 1] as 1,
-    fontWeight: 'bold'
-  };
-
-  acc[`v${value}n` as `v${1}n`] = {
-    fontSize: fontNumbers[value - 1] as 1,
-    fontWeight: 'normal'
-  };
-
-  return acc;
-}, {} as CustomFonts);
-
 export const customFonts = {
   ...DefaultTheme.fonts,
-  ...additionalFonts
+  v1: {
+    fontSize: 1,
+    fontWeight: 'bold'
+  },
+  v1n: {
+    fontSize: 1,
+    fontWeight: 'normal'
+  },
+  v2: {
+    fontSize: 2,
+    fontWeight: 'bold'
+  },
+  v2n: {
+    fontSize: 2,
+    fontWeight: 'normal'
+  },
+  v3: {
+    fontSize: 3,
+    fontWeight: 'bold'
+  },
+  v3n: {
+    fontSize: 3,
+    fontWeight: 'normal'
+  },
+  v4: {
+    fontSize: 4,
+    fontWeight: 'bold'
+  },
+  v4n: {
+    fontSize: 4,
+    fontWeight: 'normal'
+  },
+  v5: {
+    fontSize: 5,
+    fontWeight: 'bold'
+  },
+  v5n: {
+    fontSize: 5,
+    fontWeight: 'normal'
+  },
+  v6: {
+    fontSize: 6,
+    fontWeight: 'bold'
+  },
+  v6n: {
+    fontSize: 6,
+    fontWeight: 'normal'
+  },
+  v7: {
+    fontSize: 7,
+    fontWeight: 'bold'
+  },
+  v7n: {
+    fontSize: 7,
+    fontWeight: 'normal'
+  },
+  v8: {
+    fontSize: 8,
+    fontWeight: 'bold'
+  },
+  v8n: {
+    fontSize: 8,
+    fontWeight: 'normal'
+  },
+  v9: {
+    fontSize: 9,
+    fontWeight: 'bold'
+  },
+  v9n: {
+    fontSize: 9,
+    fontWeight: 'normal'
+  },
+  v10: {
+    fontSize: 10,
+    fontWeight: 'bold'
+  },
+  v10n: {
+    fontSize: 10,
+    fontWeight: 'normal'
+  },
+  v11: {
+    fontSize: 11,
+    fontWeight: 'bold'
+  },
+  v11n: {
+    fontSize: 11,
+    fontWeight: 'normal'
+  },
+  v12: {
+    fontSize: 12,
+    fontWeight: 'bold'
+  },
+  v12n: {
+    fontSize: 12,
+    fontWeight: 'normal'
+  },
+  v13: {
+    fontSize: 13,
+    fontWeight: 'bold'
+  },
+  v13n: {
+    fontSize: 13,
+    fontWeight: 'normal'
+  },
+  v14: {
+    fontSize: 14,
+    fontWeight: 'bold'
+  },
+  v14n: {
+    fontSize: 14,
+    fontWeight: 'normal'
+  },
+  v15: {
+    fontSize: 15,
+    fontWeight: 'bold'
+  },
+  v15n: {
+    fontSize: 15,
+    fontWeight: 'normal'
+  },
+  v16: {
+    fontSize: 16,
+    fontWeight: 'bold'
+  },
+  v16n: {
+    fontSize: 16,
+    fontWeight: 'normal'
+  },
+  v17: {
+    fontSize: 17,
+    fontWeight: 'bold'
+  },
+  v17n: {
+    fontSize: 17,
+    fontWeight: 'normal'
+  },
+  v18: {
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+  v18n: {
+    fontSize: 18,
+    fontWeight: 'normal'
+  },
+  v19: {
+    fontSize: 19,
+    fontWeight: 'bold'
+  },
+  v19n: {
+    fontSize: 19,
+    fontWeight: 'normal'
+  },
+  v20: {
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
+  v20n: {
+    fontSize: 20,
+    fontWeight: 'normal'
+  },
+  v21: {
+    fontSize: 21,
+    fontWeight: 'bold'
+  },
+  v21n: {
+    fontSize: 21,
+    fontWeight: 'normal'
+  },
+  v22: {
+    fontSize: 22,
+    fontWeight: 'bold'
+  },
+  v22n: {
+    fontSize: 22,
+    fontWeight: 'normal'
+  },
+  v23: {
+    fontSize: 23,
+    fontWeight: 'bold'
+  },
+  v23n: {
+    fontSize: 23,
+    fontWeight: 'normal'
+  },
+  v24: {
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  v24n: {
+    fontSize: 24,
+    fontWeight: 'normal'
+  },
+  v25: {
+    fontSize: 25,
+    fontWeight: 'bold'
+  },
+  v25n: {
+    fontSize: 25,
+    fontWeight: 'normal'
+  }
 };
--- 0.71.8/AwesomeProject/z-tasks.md
+++ 0.71.8/AwesomeProject/z-tasks.md
@@ -1,11 +1,7 @@
 # Tasks:
 
-- [x]: read the value of theme from Async storage
-- [x]: theme is stored using Aync storage
-- [ ]: Splash: edits the theme
 - [ ]: Navigations TS
 - [ ]: Main Layout
 - [ ]: Components : AppBar
 - [ ]: Components : TouchFiller
 - [ ]: i18n
-- [ ]: Touch Filler
