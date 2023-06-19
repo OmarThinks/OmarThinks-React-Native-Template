de7faf28b88bc2d8c2134f0b084640863cec7c6c
OmarThinks <24939461+OmarThinks@users.noreply.github.com>
2023-06-19 18:41:37 +0300

    Components1: added to navigations

--- 0.71.8/AwesomeProject/src/navigation/RootStack.tsx
+++ 0.71.8/AwesomeProject/src/navigation/RootStack.tsx
@@ -1,12 +1,13 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Home, Splash } from '@screens';
+import { Home, Splash, Components1 } from '@screens';
 import React from 'react';
 
 // https://reactnavigation.org/docs/typescript/
 
 export const navigationNames = {
   Home: 'Home',
-  Splash: 'Splash'
+  Splash: 'Splash',
+  Components1: 'Components1'
 } as const;
 
 export type NavigationNameType = keyof typeof navigationNames;
@@ -14,6 +15,7 @@ export type NavigationNameType = keyof typeof navigationNames;
 export type RootStackParamList = {
   [navigationNames.Home]: undefined;
   [navigationNames.Splash]: undefined;
+  [navigationNames.Components1]: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -24,5 +26,6 @@ export const RootStack = () => (
     screenOptions={{ headerShown: false }}>
     <Stack.Screen name={navigationNames.Splash} component={Splash} />
     <Stack.Screen name={navigationNames.Home} component={Home} />
+    <Stack.Screen name={navigationNames.Components1} component={Components1} />
   </Stack.Navigator>
 );
