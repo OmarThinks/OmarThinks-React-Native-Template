67c40a78b6eeb0f1171b75aa2214671008a6d745
OmarThinks <24939461+OmarThinks@users.noreply.github.com>
2023-06-22 22:25:55 +0300

    installed types for RN vector icons

--- 0.71.8/AwesomeProject/src/components/Icon/Icon.tsx
+++ 0.71.8/AwesomeProject/src/components/Icon/Icon.tsx
@@ -2,10 +2,13 @@ import { View } from 'react-native';
 import React from 'react';
 import { Text } from '@components';
 
+import FAIcon from 'react-native-vector-icons/FontAwesome';
+
 const Icon = () => {
   return (
     <View>
       <Text>Icon</Text>
+      <FAIcon name="rocket" size={30} color="#900" />
     </View>
   );
 };
