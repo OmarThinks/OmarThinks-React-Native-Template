50637236d7ca2bdc5becae1358db8291fc0e95b1
OmarThinks <24939461+OmarThinks@users.noreply.github.com>
2023-08-24 08:51:11 +0300

    AppBar: migrated to NativeWind

--- 0.72.1/AwesomeProject/src/components/AppBar/AppBar.tsx
+++ 0.72.1/AwesomeProject/src/components/AppBar/AppBar.tsx
@@ -1,15 +1,15 @@
-import { Text } from '@components';
-import { setTheme, themeSelector } from '@redux';
-import { useAppTheme } from '@theme';
+import {Text} from '@components';
+import {setTheme, themeSelector} from '@redux';
+import {useAppTheme} from '@theme';
 import React from 'react';
-import { View } from 'react-native';
-import { useDispatch, useSelector } from 'react-redux';
+import {View} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
 
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { RootStackParamList, navigationNames } from '@navigation';
-import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
+import {RootStackParamList, navigationNames} from '@navigation';
+import {useNavigation} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 
-const AppBar = ({ title }: {title?: string}) => {
+const AppBar = ({title}: {title?: string}) => {
   const colors = useAppTheme().colors;
   const dispatch = useDispatch();
   const theme = useSelector(themeSelector);
@@ -21,47 +21,28 @@ const AppBar = ({ title }: {title?: string}) => {
     dispatch(setTheme(theme === 'light' ? 'dark' : 'light'));
   };
 
-  // console.log('Title', title);
-
   return (
     <View
-      style={{
-        height: 50,
-        backgroundColor: colors.appBarBg,
-        paddingHorizontal: 10,
-        alignSelf: 'stretch',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between'
-        // flexGrow: 1,
-        // flexShrink: 1
-      }}>
-      <View
-        style={{
-          flexDirection: 'row',
-          flexGrow: 1,
-          flexShrink: 1,
-          marginRight: 20
-        }}>
+      className="h-[50] px-2 flex-row items-center justify-between self-stretch"
+      style={{backgroundColor: colors.appBarBg}}>
+      <View className="flex-row grow shrink mr-5">
         <Text
-          style={{
-            color: colors.normalText,
-            flexShrink: 1
-          }}
+          className="shrink"
+          style={{color: colors.normalText}}
           variant="v20"
           numberOfLines={1}>
           {title}
         </Text>
       </View>
-      <View style={{ flexDirection: 'row' }}>
+      <View className="flex-row">
         <Text
-          style={{ marginRight: 10 }}
+          className="mr-2"
           onPress={() => {
             navigation.navigate(navigationNames.Components1);
           }}>
           Components 1
         </Text>
-        <Text style={{ marginRight: 10 }} onPress={toggleTheme}>
+        <Text className="mr-2" onPress={toggleTheme}>
           Theme
         </Text>
         <Text style={{}}>Drawer</Text>
