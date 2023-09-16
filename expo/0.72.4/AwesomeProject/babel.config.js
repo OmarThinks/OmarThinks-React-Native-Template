3202daace49fa076868d5c230697d595d214d580
OmarThinks <24939461+OmarThinks@users.noreply.github.com>
2023-09-16 21:44:33 +0300

    pasted folders from cli and resolved modules

--- expo/0.72.4/AwesomeProject/babel.config.js
+++ expo/0.72.4/AwesomeProject/babel.config.js
@@ -11,13 +11,15 @@ module.exports = function (api) {
           root: ["."],
           alias: {
             "@components": "./src/components/index.tsx",
-            //"@hoc": "./src/hoc/index.tsx",
-            //"@redux": "./src/redux/index.tsx",
+            "@hoc": "./src/hoc/index.tsx",
+            "@redux": "./src/redux/index.tsx",
+            "@storage": "./src/storage/index.tsx",
+            "@theme": "./src/theme/index.tsx",
+            "@locale": "./src/locale/index.tsx",
+
             //"@screens": "./src/screens/index.tsx",
-            //"@storage": "./src/storage/index.tsx",
-            //"@theme": "./src/theme/index.tsx",
             //"@navigation": "./src/navigation/index.tsx",
-            //"@locale": "./src/locale/index.tsx",
+            // '@app': './src/App.tsx' (Only if needed)
           },
         },
       ],
