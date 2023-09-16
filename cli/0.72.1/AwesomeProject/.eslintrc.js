d8176676f99005b643f89fe5628444ba055059d8
OmarThinks <24939461+OmarThinks@users.noreply.github.com>
2023-09-16 18:03:23 +0300

    EsLint: BugFix: End of line

--- cli/0.72.1/AwesomeProject/.eslintrc.js
+++ cli/0.72.1/AwesomeProject/.eslintrc.js
@@ -4,5 +4,11 @@ module.exports = {
   rules: {
     'react-native/no-inline-styles': 0,
     'no-multiple-empty-lines': ['error', {max: 2}],
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
 };
